924e760feb1ff7642148132ce31aeb0985dad53a
Claudio Siqueira <55467425+ClaudioSiqueira@users.noreply.github.com>
2020-09-08 18:29:35 -0300

    Preparando ambiente

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) =>{ // it's an event, the socket is the client that is connected to the server
+    socket.on('disconnect', () =>{
+        console.log('id: ' + socket.id + ' se desconectou')
+    })
+
+    socket.on('msg', (data) =>{
+        socket.emit('showmsg', data)
+        console.log(data)
+    })
+})
+
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) =>{
+    res.render('index')
+})
+
+http.listen(3000, () =>{
+    console.log('Servidor aberto !')
+})
